fe7a7527f54c59168c237f268db2c322a0c71793
laurenmackhicks <laurenhouston@live.com>
2021-06-22 16:53:39 +0900

    Prophet Changes

--- lesson9/prophets/prophets.js
+++ lesson9/prophets/prophets.js
@@ -13,13 +13,11 @@ fetch(requestURL)
         let h2 = document.createElement('h2');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
-        let p3 = document.createElement('p');
         let image = document.createElement('img');
 
         h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
-        p1.textContent = prophets[i].birthdate + ' - ' + prophets[i].death;
-        p2.innerHTML = `Birthplace: ${prophets[i].birthplace} </br> Number of Children: ${prophets[i].numofchildren}`; 
-        p3.innerHTML = `Years as Prophet: ${prophets[i].length} </br> Number in Order: ${prophets[i].order} `;
+        p1.textContent = prophets[i].birthdate; 
+        p2.innerHTML = `Birthplace: ${prophets[i].birthplace}`; 
         image.setAttribute('src', prophets[i].imageurl);
         image.setAttribute('alt', `The prophet ${prophets[i].name} ${prophets[i].lastname}!`)
         image.style.boxShadow = '0 0 30px #999'
@@ -28,7 +26,6 @@ fetch(requestURL)
         card.appendChild(p1);
         card.appendChild(p2);
         card.appendChild(image);
-        card.appendChild(p3);
 
         document.querySelector('div.cards').appendChild(card);
     }
